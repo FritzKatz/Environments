c0afd84e5949552be8d35e6216f61fb6edc1d828
FritzKatz <fritz_katzfuss@hotmail.com>
2015-08-26 21:51:29 +0200

    Created initial MonitoringApp

---
+++ js/app.js
@@ -0,0 +1,44 @@
+(function (){
+	var app = angular.module('envs', []);
+
+/*	app.controller('EnvsController', function(){
+		this.servers = data;
+	});*/
+
+	
+
+	app.controller('EnvsController', ['$scope', '$http', function($scope, $http) {
+		$http.get('json/store.json').success(function(data) {
+			$scope.servers = data;
+		});		
+	}]);
+
+
+/*    app.controller('EnvsController', ['$scope', '$http', function($scope, $http) {
+        $scope.servers=[];
+        $http.get('xml/BMW.digital_DEV2.xml').success(function(data) {
+            $scope.servers = $.xml2json(data,true);
+        });
+    }]);*/
+
+    app.directive('popover', function() {
+        return function() {
+            $("[data-toggle='popover']").popover({
+                //trigger: 'click focus'
+            });
+
+            $('body').on('click', function (element) {
+                $("[data-toggle='popover']").each(function () {
+                    //the 'is' for buttons that trigger popups
+                    //the 'has' for icons within a button that triggers a popup
+                    if (!$(this).is(element.target) && $(this).has(element.target).length === 0 && $('.popover').has(element.target).length === 0) {
+                        $(this).popover('hide');
+                    }
+                });
+            });
+
+        }
+    });
+
+
+})();
